5684cb600f064c520a44ee1343fc77da14d6da33
eespeed <itseverydaybro263@gmail.com>
2023-11-09 21:53:11 -0600

    Added functionality for translated dialogue options
    -created LanguageSelector class that uses a dictionary for each language
    -made slight edits to Jay's code in FrmLevel so it utilizes LanguageSelector
    -the text is not fully translated; just placeholder text for now (besides English)

--- Project/Fall2020_CSC403_Project/LanguageSelectScreen.cs
+++ Project/Fall2020_CSC403_Project/LanguageSelectScreen.cs
@@ -15,10 +15,6 @@ namespace Fall2020_CSC403_Project
 {
     public partial class LanguageSelectScreen : Form
     {
-        public bool englishSelected = false;
-        bool spanishSelected = false;
-        bool germanSelected = false;
-        bool latinSelected = false;
         public LanguageSelectScreen()
         {
             InitializeComponent();
@@ -31,31 +27,29 @@ namespace Fall2020_CSC403_Project
 
         private void EnglishButtonClick(object sender, EventArgs e)
         {
-            englishSelected = true;
-            button1.Click += EnglishButtonClick;
-
-            FrmLevel languageSelectScreen = new FrmLevel();
+            LanguageSelector selector = new LanguageSelector();
+            FrmLevel languageSelectScreen = new FrmLevel(selector, "English");
             languageSelectScreen.Show();
         }
 
         private void SpanishButtonClick(object sender, EventArgs e)
         {
-            spanishSelected = true;
-            FrmLevel languageSelectScreen = new FrmLevel();
+            LanguageSelector selector = new LanguageSelector();
+            FrmLevel languageSelectScreen = new FrmLevel(selector, "Spanish");
             languageSelectScreen.Show();
         }
 
         private void GermanButtonClick(object sender, EventArgs e)
         {
-            germanSelected = true;
-            FrmLevel languageSelectScreen = new FrmLevel();
+            LanguageSelector selector = new LanguageSelector();
+            FrmLevel languageSelectScreen = new FrmLevel(selector, "German");
             languageSelectScreen.Show();
         }
 
         private void LatinButtonClick(object sender, EventArgs e)
         {
-            latinSelected = true;
-            FrmLevel languageSelectScreen = new FrmLevel();
+            LanguageSelector selector = new LanguageSelector();
+            FrmLevel languageSelectScreen = new FrmLevel(selector, "Latin");
             languageSelectScreen.Show();
         }
     }
---
+++ Project/Fall2020_CSC403_Project/LanguageSelector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project
+{
+    public class LanguageSelector
+    {
+        public Dictionary<string, List<string>> EnglishDialogue;
+        public Dictionary<string, List<string>> SpanishDialogue;
+        public Dictionary<string, List<string>> GermanDialogue;
+        public Dictionary<string, List<string>> LatinDialogue;
+
+        public LanguageSelector()
+        {
+            EnglishDialogue = new Dictionary<string, List<string>>();
+            SpanishDialogue = new Dictionary<string, List<string>>();
+            GermanDialogue = new Dictionary<string, List<string>>();
+            LatinDialogue = new Dictionary<string, List<string>>();
+
+            EnglishDialogue.Add("koolaidManDialogue", new List<string> { "You hear a a slight rumble...", "Koolaid Man breaks through the wall", "\"OHH, YEAH\"" });
+            SpanishDialogue.Add("koolaidManDialogue", new List<string> { "Escuchas un leve retumbar...", "Koolaid Man rompe la pared", "\"¡OHH, SÍ!\"" });
+            GermanDialogue.Add("koolaidManDialogue", new List<string> { "German text placeholder Kool Aid Man", "German text placeholder 2 Kool Aid Man" });
+            LatinDialogue.Add("koolaidManDialogue", new List<string> { "Latin text placeholder Kool Aid Man", "Latin text placeholder 2 Kool Aid Man" });
+
+            EnglishDialogue.Add("poisonKoolaidDialogue", new List<string> { "neurotoxins" });
+            SpanishDialogue.Add("poisonKoolaidDialogue", new List<string> { "Spanish text placeholder Poison Packet" });
+            GermanDialogue.Add("poisonKoolaidDialogue", new List<string> { "German text placeholder Poison Packet" });
+            LatinDialogue.Add("poisonKoolaidDialogue", new List<string> { "Latin text placeholder Poison Packet" });
+
+            EnglishDialogue.Add("cheetoDialogue", new List<string> { "HOW DO YOU EXPECT TO FIGHT", "WHEN YOUR HANDS ARE COVERED IN CHEETO DUST!" });
+            SpanishDialogue.Add("cheetoDialogue", new List<string> { "Spanish text placeholder Cheeto", "Spanish text placeholder 2 Cheeto" });
+            GermanDialogue.Add("cheetoDialogue", new List<string> { "German text placeholder Cheeto", "German text placeholder 2 Cheeto" });
+            LatinDialogue.Add("cheetoDialogue", new List<string> { "Latin text placeholder Cheeto", "Latin text placeholder 2 Cheeto" });
+
+        }
+
+        public List<string> GetDialogue(string key, string language)
+        {
+            if (language == "English")
+            {
+                return EnglishDialogue[key];
+            }
+            else if (language == "Spanish")
+            {
+                return SpanishDialogue[key];
+            }
+            else if (language == "German")
+            {
+                return GermanDialogue[key];
+            }
+            else if (language == "Latin")
+            {
+                return LatinDialogue[key];
+            }
+            return null;
+        }
+    }
+}
